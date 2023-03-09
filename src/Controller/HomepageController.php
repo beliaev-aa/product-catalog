999cf35fc92066a4b6e175fc7b36c36e3cc42f86
Aleksei Beliaev <virtual4all@mail.ru>
2023-03-09 22:03:58 +0300

    To test the functionality of twig, the free Estore template has been added without any optimization, as is.

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,11 +4,24 @@ declare(strict_types=1);
 
 namespace Onlydev\Controller;
 
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
 class HomepageController extends AbstractController
 {
+    /**
+     * @throws NotFoundExceptionInterface
+     * @throws SyntaxError
+     * @throws ContainerExceptionInterface
+     * @throws RuntimeError
+     * @throws LoaderError
+     */
     public function index(): void
     {
-        phpinfo();
+        $this->render('homepage/index.html.twig');
     }
 
     public function hello(string $name): void
